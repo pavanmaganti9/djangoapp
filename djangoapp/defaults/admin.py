722518a1ae18335d27f1c9026b7272b23001ce30
PVPavanMaganti <pavan@markitome.com>
2019-10-03 00:23:09 +0530

    form fields

--- djangoapp/defaults/admin.py
+++ djangoapp/defaults/admin.py
@@ -5,10 +5,10 @@ from .models import blog
 
 class BlogAdmin(admin.ModelAdmin):
 	search_fields = ['title','tag']
-	list_display = ['title','tag','author']
+	list_display = ['title','email','tag','author','date']
 	list_editable = ['tag']
 	list_filter = ['tag']
-	readonly_fields = ['tag']
+	#readonly_fields = ['tag']
 	#prepopulated_fields = {"slug": ['tag']}
 # Register your models here.
 admin.site.register(blog, BlogAdmin)
--- djangoapp/defaults/models.py
+++ djangoapp/defaults/models.py
@@ -1,10 +1,23 @@
 from django.db import models
+from datetime import datetime
+
+
+COLOR_CHOICES = [
+    ('blue', 'Blue'),
+    ('green', 'Green'),
+    ('black', 'Black'),
+]
+
 
 # Create your models here.
 class blog(models.Model):
-    title = models.CharField(max_length=400)
-    tag = models.CharField(max_length=50)
-    author = models.CharField(max_length=120)
-		
-    def __unicode__(self):
-        return str(self.title)
+	title = models.CharField(max_length=400)
+	email = models.EmailField(max_length=50, blank=True)
+	tag = models.CharField(max_length=50)
+	author = models.CharField(max_length=120)
+	color = models.CharField(max_length=6, choices=COLOR_CHOICES)
+	#gender = forms.ChoiceField(choices=GENDER_CHOICES,widget=forms.RadioSelect())
+	date = models.DateTimeField(default=datetime.now(), blank=True)
+	
+	def __str__(self):
+		return str(self.title)
